a12aed6de7087ecc361a12356acb76ec7e13c717
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-06-30 04:10:37 +0400

    Update store.js

--- app/store/store.js
+++ app/store/store.js
@@ -1,12 +1,12 @@
 "use client"
 import { configureStore } from '@reduxjs/toolkit';
-import navigationReducer from './features/navigationSlice';
+import drivers from './features/drivers';
 import checkoutReducer from './features/checkout'
 import  cart from "./features/cart"
 import wishlist from "./features/wishlist"
 const store = configureStore({
     reducer: {
-        navigation: navigationReducer,
+        drivers: drivers,
         checkout: checkoutReducer,
         cart:cart,
         wishlist:wishlist
