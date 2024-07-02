888ce63abf3a65fc309e9801d553c75e5c19b36f
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-07-03 00:42:06 +0400

    Update drivers

---
+++ app/drivers/notes/page.js
@@ -0,0 +1,13 @@
+export default function page(){
+return  <div class="p-3 bg-white space-y-4">
+
+  <div class="rounded-lg p-2 py-5 rounded-lg shadow-md relative overflow-hidden">
+    <p class="text-sm text-gray-500">تبديل الشفت من الفترة المسائية الى الفترة الصباحية</p>
+
+    <span class="absolute px-1.5  bottom-0 right-0 bg-yellow-600 text-white  text-xs">2022/8/6</span>
+  </div >
+
+
+</div>
+
+}
