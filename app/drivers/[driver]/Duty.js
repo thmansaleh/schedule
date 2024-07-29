6dc7ea2900c0c2ebad4b485a13908a52e2c2ca89
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-07-29 19:41:26 +0400

    Update Duty.js

--- app/drivers/[driver]/Duty.js
+++ app/drivers/[driver]/Duty.js
@@ -1,37 +1,36 @@
-import { updateDriver } from "@/app/store/features/driver"
-import { duty } from "@/app/swr/duty"
-import { useDispatch, useSelector } from "react-redux"
+import { updateDriver } from "@/app/store/features/driver";
+import { duty } from "@/app/swr/duty";
+import { useDispatch, useSelector } from "react-redux";
 
 function Duty() {
-    const {data,isLoading,error} =duty()
-    const dispatch =useDispatch()
-    const driver =useSelector(state=>state.driver.updateDriver)
+    const { data, isLoading, error } = duty();
+    const dispatch = useDispatch();
+    const driver = useSelector((state) => state.driver.updateDriver);
 
-    if(isLoading) return 'loading' 
-    if(error) return 'errore' 
-  return (
-    <div>
-      <label htmlFor="countries_disabled" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">فترة العمل</label>
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error occurred</div>;
 
-      <select 
-       onChange={(e)=>{
-        dispatch(updateDriver({action:'duty',value:e.target.value}))
-      }}
+    return (
+        <div>
+            <label htmlFor="countries_disabled" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                فترة العمل
+            </label>
 
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
-       {
-data.map((e,i)=>{
-
-
-return <option  selected={e.id===driver.duty_id?true:false} key={i} value={e.id}>{e.name}</option>
-})}
-
-      </select>
-
-
-
-
-    </div>  )
+            <select
+                onChange={(e) => {
+                    dispatch(updateDriver({ action: "duty", value: e.target.value }));
+                }}
+                defaultValue={driver.duty_id}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            >
+                {data.map((e) => (
+                    <option key={e.id} value={e.id}>
+                        {e.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
 }
 
-export default Duty
+export default Duty;
