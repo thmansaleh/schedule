e4e6b4fc2e9e3ca4b7f2665324a640bfeeace043
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-08-09 18:51:57 +0400

    Update services

---
+++ app/services/reoprts.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+ const url ="https://express-rta.vercel.app/get-reports-by-job-id"
+
+  export const login= async (start,end,jobId) => {
+   const response = await axios.get(url,{
+     params: {
+       date_start:start,
+       date_end:end,
+       job_id:jobId
+      } });
+
+   return response.data;
+ };
