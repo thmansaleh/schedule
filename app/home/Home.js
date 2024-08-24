eebdc41fa914f815164d66deabddb10845b83d3a
othman <thman.saleh@gmail.com>
2024-08-25 00:55:56 +0400

    t4gfr4f

--- app/home/Home.js
+++ app/home/Home.js
@@ -48,7 +48,7 @@ route:"./patrols/select"
 
 
 ]
-  return <div className="flex items-center justify-center h-screen bg-gray-100">
+  return <div className="flex items-center justify-center h-screen bg-white">
     <div className="flex gap-10 p-4 items-center justify-center flex-wrap ">
 {/* {data.map((e,i)=>{
  return   <Link href={e.route} key={i} className="w-24  h-24 bg-white  rounded-lg flex items-center
@@ -62,8 +62,7 @@ shadow-lg p-4 hover:mb-4  justify-around flex-col ">
 })} */}
 <Link href='./drivers' className="flex flex-col items-center gap-y-1 justify-center">
 <div className="p-5  text-green-500  bg-green-100 flex items-center justify-center rounded-full">
-<svg className="w-7 h-7 "  stroke="currentColor" fill="currentColor" height={512} viewBox="0 0 24 24" width={512} xmlns="http://www.w3.org/2000/svg" data-name="Layer 1"><path d="m7.5 13a4.5 4.5 0 1 1 4.5-4.5 4.505 4.505 0 0 1 -4.5 4.5zm6.5 11h-13a1 1 0 0 1 -1-1v-.5a7.5 7.5 0 0 1 15 0v.5a1 1 0 0 1 -1 1zm3.5-15a4.5 4.5 0 1 1 4.5-4.5 4.505 4.505 0 0 1 -4.5 4.5zm-1.421 2.021a6.825 6.825 0 0 0 -4.67 2.831 9.537 9.537 0 0 1 4.914 5.148h6.677a1 1 0 0 0 1-1v-.038a7.008 7.008 0 0 0 -7.921-6.941z" /></svg>
-
+<svg className="w-7 h-7 "  stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 640 512" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z"></path></svg>
 </div>
 <div className=" text-sm font-semibold">خبراء السير</div>
 </Link>
