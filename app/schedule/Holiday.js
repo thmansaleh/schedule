a13f1899af5fefae59f312eb6d1c83ad7e8c0c0e
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-08-25 14:35:51 +0400

    Update Holiday.js

--- app/schedule/Holiday.js
+++ app/schedule/Holiday.js
@@ -9,7 +9,7 @@ export default function Holiday({shiftOffId,driverData}){
    let count =0
     return <div style={{color:color?color:'black'}} className="px-4">
 <h1 className="text-center py-2 text-base  font-semibold">الأفراد تحت الجاهزية </h1>
-  {driverData.sort((a, b) => a.name.localeCompare(b.name, 'ar')).map((e,i)=>{
+  {driverData.map((e,i)=>{
     if(e.status=="true"&&e.shift_id==shiftOffId) {
    count+=1
  return <div key={i} className="py-px flex items-center gap-x-2">
