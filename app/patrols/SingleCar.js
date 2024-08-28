75a65dcd8e1821a62aa00c7c8f3f7fdfe9719729
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-08-28 18:08:04 +0400

    Update SingleCar.js

--- app/patrols/SingleCar.js
+++ app/patrols/SingleCar.js
@@ -7,12 +7,12 @@ function SingleCar({car}) {
 <CarModal car={car}/>
 <div>
     {/* {car.is_match&&car.drivers.map(driver=>{
-        return <div className='text-sm font-semibold ' key={driver.id=Math.random()}> {driver.name}</div>
+        return <div className='text-sm  ' key={driver.id=Math.random()}> {driver.name}</div>
     })} */}
-{car.is_match?<div>{car.driver_name}</div>:null}
-{car.extra_driver_name?<div>{car.extra_driver_name}</div>:null}
+{car.is_match?<div className='text-sm  '>{car.driver_name}</div>:null}
+{car.extra_driver_name?<div className='text-sm  '>{car.extra_driver_name}</div>:null}
 </div>
-{car.is_match?<div>{new Date(car.date).toLocaleString()}</div>:null
+{car.is_match?<div className='text-sm  '>{new Date(car.date).toLocalTimeString()}</div>:null
 }
     </div>
   )
